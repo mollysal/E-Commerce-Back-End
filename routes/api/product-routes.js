47a837f3515635986631e4b4f0a3220c4bf92a47
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-04 19:39:48 -0400

    Finishing up Routes need to test

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      include: [{ model: Category}, { model: Tag }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findByPk({
+      include: [{ model: Category}, { model: Tag }]
+    });
+    if (!productData) {
+      res.status(400).json({ message: 'No Product found with this id' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +112,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData) {
+      res.status(404).json({message: 'No Product found with that ID'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
