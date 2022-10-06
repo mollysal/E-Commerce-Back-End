255493e5a91e03255e8dbb2b905758e410d60c46
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-10-05 23:43:51 -0400

    Looks like everything is working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,8 +106,9 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
+      //console.log(err);
+      //res.status(400).json(err);
+      res.json(err);
     });
 });
 
